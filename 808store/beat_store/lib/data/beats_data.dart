7754c586e13ac183eddee4accfbf16b2e9c9d3b7
unknown <wrzos04@gmail.com>
2023-03-16 12:38:57 +0100

    added beat data

---
+++ 808store/beat_store/lib/data/beats_data.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:beat_store/components/beat_list_element.dart';
+
+const beatsData = [
+  BeatListElement(
+      beatPhotoPath: "assets/images/4pf.jpg",
+      beatTitle: "\"4PF\" - Lil Baby Type Beat",
+      beatBPM: "144 BPM",
+      beatScale: "D# Min"),
+  BeatListElement(
+      beatPhotoPath: "assets/images/808mafia.jpg",
+      beatTitle: "\"SOUTHSIDE\" - 808Mafia Type Beat",
+      beatBPM: "163 BPM",
+      beatScale: "D Min"),
+  BeatListElement(
+      beatPhotoPath: "assets/images/jersey.jpg",
+      beatTitle: "\"ROCK\" - Jersey Club Type Beat",
+      beatBPM: "140 BPM",
+      beatScale: "C# Min"),
+  BeatListElement(
+      beatPhotoPath: "assets/images/supertrap.jpg",
+      beatTitle: "\"RIZZ\" - Supertrap Type Beat",
+      beatBPM: "132 BPM",
+      beatScale: "D# Min"),
+  BeatListElement(
+      beatPhotoPath: "assets/images/dom.jpg",
+      beatTitle: "\"Ginseng\" - Dom Corleo Type Beat",
+      beatBPM: "130 BPM",
+      beatScale: "A# Min"),
+  BeatListElement(
+      beatPhotoPath: "assets/images/destroy.jpg",
+      beatTitle: "\"VTMNTS\" - Destroy Lonely Type Beat",
+      beatBPM: "128 BPM",
+      beatScale: "F# Min"),
+  BeatListElement(
+      beatPhotoPath: "assets/images/ken.jpg",
+      beatTitle: "\"X\" - Ken Carson Type Beat",
+      beatBPM: "146 BPM",
+      beatScale: "G Min"),
+      BeatListElement(
+      beatPhotoPath: "assets/images/pop.jpg",
+      beatTitle: "\"WOO\" - Pop Smoke Type Beat",
+      beatBPM: "140 BPM",
+      beatScale: "C Min")
+];
