2aadf985fe773f183c8299e99748293f42ec7613
unknown <wrzos04@gmail.com>
2023-04-18 14:27:37 +0200

    Added licensing widgets

--- 808store/beat_store/lib/components/beat_tile.dart
+++ 808store/beat_store/lib/components/beat_tile.dart
@@ -1,4 +1,3 @@
-import 'package:beat_store/screens/category_page.dart';
 import 'package:flutter/material.dart';
 
 class BeatTile extends StatelessWidget {
@@ -14,62 +13,69 @@ class BeatTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Card(
-          elevation: 30,
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            height: 150,
-            width: 150,
-            margin: const EdgeInsets.only(right: 20),
-            decoration: BoxDecoration(
-              
-              borderRadius: BorderRadius.circular(15),
-              color: Theme.of(context).colorScheme.background,
-            ),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(licenseType,
-                      style: Theme.of(context).textTheme.headline5),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(licenseDescription,
-                          style: Theme.of(context).textTheme.headline4),
-                      const SizedBox(width: 12),
-                      const Icon(
-                        Icons.add_shopping_cart_rounded,
-                        size: 35,
-                        color: Colors.white,
-                      ),
-                    ],
+        Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              height: 150,
+              width: 170,
+              margin: const EdgeInsets.only(right: 20),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                color: Theme.of(context).colorScheme.background,
+              ),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(licenseType,
+                        style: Theme.of(context).textTheme.headline5),
                   ),
-                ),
-              ],
+                  Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(licenseDescription,
+                            style: Theme.of(context).textTheme.headline4),
+                        const Spacer(),
+                        const Icon(
+                          Icons.add_shopping_cart_rounded,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer()
+          ],
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 20,
+            width: 170,
+            margin: const EdgeInsets.only(bottom: 10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15)),
+            ),
+            child: Text(
+              beatPrice,
+              style: Theme.of(context).textTheme.headline5,
+              textAlign: TextAlign.center,
             ),
           ),
         ),
-        Container(
-          height: 20,
-          width: 110,
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primary,
-            borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(37),
-                bottomRight: Radius.circular(37)),
-          ),
-          child: Text(
-            beatPrice,
-            style: Theme.of(context).textTheme.headline5,
-            textAlign: TextAlign.center,
-          ),
-        )
       ],
     );
   }
